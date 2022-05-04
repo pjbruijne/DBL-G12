6eed8e0d45aa259c947e2e2f364906ab74a10d94
Tjeerd <j.j.b.roks@student.tue.nl>
2022-05-04 12:14:03 +0200

    Comments added

--- src/EmergencyState/LEDBlink.java
+++ src/EmergencyState/LEDBlink.java
@@ -5,6 +5,7 @@ public class LEDBlink {
     private int pinNumber; // WiringPin diagram
     private int blinkSpeed; // in miliseconds
 
+    // Constructor
     public LEDBlink(int pinNumber, int blinkSpeed) {
         this.pinNumber = pinNumber;
         this.blinkSpeed = blinkSpeed;
@@ -13,10 +14,13 @@ public class LEDBlink {
     public void Blink() {
         try {
            Runtime runTime = Runtime.getRuntime();
+           // Set the pin to receiving
             runTime.exec("gpio mode " + pinNumber + " out");
            while(true) {
+               // Set the pin to high and wait
                runTime.exec("gpio write " + pinNumber + " 1");
                Thread.sleep(blinkSpeed);
+               // Set the pin to low and wait
                runTime.exec("gpio write " + pinNumber + " 0");
                Thread.sleep(blinkSpeed);
             }
