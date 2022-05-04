e24bd151046aeb11228bc98f893d3693869af071
Peter de Bruijne <p.j.d.bruijne@student.tue.nl>
2022-05-04 11:06:49 +0200

    Updated the signal error to give advice for fixing

--- src/ErrorHandling/Errors/SignalError.java
+++ src/ErrorHandling/Errors/SignalError.java
@@ -2,6 +2,6 @@ package ErrorHandling.Errors;
 
 public class SignalError extends Error {
 	public SignalError() {
-		super("Signal Error: The RPI lost the signal to the robot or the motor. The program will therefore forcefully shut down.");
+		super("Signal Error: The RPI lost the signal to the robot or the motor. The program will therefore forcefully shut down. Try reconnecting the wires or replacing them.");
 	}
 }
