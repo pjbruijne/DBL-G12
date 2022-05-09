db278e7f8668899a416ad2529e45772c1af904f4
Peter de Bruijne <p.j.d.bruijne@student.tue.nl>
2022-05-09 14:54:07 +0200

    Updated LEDBlink to stop and Main's calls to it

--- src/EmergencyState/LEDBlink.java
+++ src/EmergencyState/LEDBlink.java
@@ -4,6 +4,7 @@ public class LEDBlink {
 
     private int pinNumber; // WiringPin diagram
     private int blinkSpeed; // in miliseconds
+    private boolean toggle = false; // to turn blinking on and off
 
     // Constructor
     public LEDBlink(int pinNumber, int blinkSpeed) {
@@ -11,12 +12,13 @@ public class LEDBlink {
         this.blinkSpeed = blinkSpeed;
     }
 
-    public void Blink() {
+    public void blink() {
         try {
            Runtime runTime = Runtime.getRuntime();
+           toggle = true;
            // Set the pin to receiving
             runTime.exec("gpio mode " + pinNumber + " out");
-           while(true) {
+           while(toggle) {
                // Set the pin to high and wait
                runTime.exec("gpio write " + pinNumber + " 1");
                Thread.sleep(blinkSpeed);
@@ -27,6 +29,15 @@ public class LEDBlink {
         } catch (Exception e) {
             // TODO: Error handling!
             System.out.println("Exception occured: " + e.getMessage());
+        }
+    }
+
+    public void stop() {
+        try {
+            toggle = false; 
+        }
+        catch (Exception e) {
+
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,41 @@ import EmergencyState.*;
 import ErrorHandling.*;
 
 public class Main {
-       
+    //The pin numbers we use for certain parts of the robot and other necessary values
+    int motorPin = 5;
+
+    int emergencyPin = 4;
+    int emergencyBlinkSpeed = 500;
+    boolean emergencyState = false;
+    LEDBlink emergencyLight;
+
+
+    public Main() {
+        emergencyLight = new LEDBlink(emergencyPin, emergencyBlinkSpeed);
+    }
+
+    void toggleEmergencyState() {
+        if (!emergencyState) {  // if there is no emergency before toggling
+            emergencyLight.blink(); // start blinking the emergency light
+            emergencyState = true;  // set the emergency state to true
+        }
+        else {  // if the emergency state is already on before toggling
+            emergencyLight.stop();  // stop the blinking
+            emergencyState = false; // set the emergency state to false
+        }
+    }
+
     public static void main(String[] args) {
         // Just a blinking led effect
         System.out.println("Press CTRL-C to exit");
+        try {
+
+        }
+        catch (Error e) {
+            LEDBlink ledBlink = new LEDBlink(4, 500);
+        }
         LEDBlink ledBlink = new LEDBlink(4, 500);
-        ledBlink.Blink();
+        ledBlink.blink();
     }
 
 }
