a2df306f4e2cb0d2b028979d8d1970830ac11df2
Peter de Bruijne <p.j.d.bruijne@student.tue.nl>
2022-05-11 12:43:57 +0200

    Updated errors and exceptions to create custom msg

--- src/ErrorHandling/Errors/BlockadeError.java
+++ src/ErrorHandling/Errors/BlockadeError.java
@@ -9,4 +9,7 @@ public class BlockadeError extends Error {
 	public BlockadeError() {
 		super("Blockade Error: The belt has been obstructed, thus no disc can pass through. For the safety of the system, the motor will turn off. Remove anything blocking the discs.");
 	}
+	public BlockadeError(String errorMsg) {
+		super(errorMsg);
+	}
 }
