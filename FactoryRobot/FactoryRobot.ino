5abdc15ae978d4367844ecfe023627358069f622
Peter de Bruijne <p.j.d.bruijne@student.tue.nl>
2022-05-30 13:55:42 +0200

    Allowed it to protothread

--- FactoryRobot/FactoryRobot.ino
+++ FactoryRobot/FactoryRobot.ino
@@ -4,36 +4,20 @@
 AF_DCMotor arm_motor(2);
 AF_DCMotor slide_motor(1);
 boolean armEnable;
+boolean slideEnable;
+const int armSpeed = 255;
 const int armDelay = 255;
+const int armFreq = 500;
+const int slideSpeed= 255;
 const int slideDelay = 300;
-
-void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Setup!");
-
-  armEnable = true;
-  startup();
-  Serial.println("startup complete!");
-  delay(1000);
-}
-
-void loop() {
-  if(armEnable) {
-    slideMove();
-    armEnable = false;
-  }
-
-  delay(1000);
-  armEnable = true;
-}
 
 void startup() {
   // turn on motor
-  arm_motor.setSpeed(255);
+  arm_motor.setSpeed(armSpeed);
   arm_motor.run(RELEASE);
 
   // turn on motor
-  slide_motor.setSpeed(255);
+  slide_motor.setSpeed(slideSpeed);
   slide_motor.run(RELEASE);
   
   arm_motor.run(FORWARD);
@@ -44,21 +28,53 @@ void startup() {
 }
 
 void armMove() {
-  arm_motor.run(FORWARD);
-  delay(armDelay);
-  arm_motor.run(BACKWARD);
-  delay(armDelay);
-  arm_motor.run(RELEASE);
-  Serial.println("Arm moving!");
+  if (armEnable) {
+    arm_motor.run(FORWARD);
+    delay(armDelay);
+    arm_motor.run(BACKWARD);
+    delay(armDelay);
+    arm_motor.run(RELEASE);
+    Serial.println("Arm moving!");
+    armEnable = false;
+  }
 }
 
 void slideMove() {
-  slide_motor.run(BACKWARD);
-  delay(slideDelay);
-  slide_motor.run(RELEASE);
-  delay(700);
-  slide_motor.run(FORWARD);
-  delay(slideDelay - 7);
-  slide_motor.run(RELEASE);
-  Serial.println("Slide moving!");
+  if (slideEnable) {
+    slide_motor.run(BACKWARD);
+    delay(slideDelay);
+    slide_motor.run(RELEASE);
+    delay(700);
+    slide_motor.run(FORWARD);
+    delay(slideDelay - 7);
+    slide_motor.run(RELEASE);
+    Serial.println("Slide moving!");
+    slideEnable = false;
+  }
+}
+
+TimedAction armThread = TimedAction(armFreq,armMove);
+TimedAction slideThread = TimedAction(slideFreq,slideMove);
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Setup!");
+
+  armEnable = true;
+  slideEnable = true;
+  startup();
+  Serial.println("startup complete!");
+  delay(1000);
+}
+
+void loop() {
+  armThread.check();
+  slideThread.check();
+  if(armEnable) {
+    slideMove();
+    armEnable = false;
+  }
+
+  delay(1000);
+  armEnable = true;
 }
