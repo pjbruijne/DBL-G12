425601d8d32a292631333158c29b7c5b57d5adec
Peter de Bruijne <p.j.d.bruijne@student.tue.nl>
2022-06-13 17:03:23 +0200

    Splitted software and arm reaction to sensor

--- FactoryRobot/FactoryRobot.ino
+++ FactoryRobot/FactoryRobot.ino
@@ -2,56 +2,38 @@
 #include "src/TimedAction/TimedAction.h"
 #include "src/Adafruit_TCS34725/Adafruit_TCS34725.h"
 
+// initialization of the motors
 AF_DCMotor arm_motor(1);    // motor 1 is assigned to the arm
 AF_DCMotor slide_motor(2);  // motor 2 is assigned to the slide
 
+// booleans for the motors
 boolean armEnable;    // global boolean for whether the arm should move
 boolean slideEnable;  // global boolean for whether the slide should move
 
+// directions of the motors, for referencing
 int armDir;   // global variable for the direction of the arm {-1 means backward, 0 means no movement, 1 means forward}
 int slideDir; // global variable for the direction of the slide {-1 means backward, 0 means no movement, 1 means forward}
 
+// arm motor variables
 const int armSpeed = 255;           // global variable for the rotating speed of the arm motor
-const unsigned long armDelay = 220; // the time the arm has to move before switching directions
+const unsigned long armDelay = 180; // the time the arm has to move before switching directions
 unsigned long armStartMillis = 0;   // the starting time of the armMove process, 0 means no process
 const int armFreq = 1;              // the wait time in milliseconds before the TimedAction should try to refire the armMove method
 
+// slide motor variables
 const int slideSpeed= 255;            // global variable for the rotating speed of the slide motor
 const unsigned long slideDelay = 300; // the time the slide has to move before stopping          
 const unsigned long slideWait = 700;  // the time the slide has to stay in place
 unsigned long slideStartMillis = 0;   // the starting time of the slideMove process, 0 means no process
 const int slideFreq = 1;              // the wait time in milliseconds before the TimedAction should try to refire the slideMove method
 
+// sensor variables
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X); // the sensor variable
-int acceptance = 150;                                                                        // the euclidean distance acceptance variable (distance < acceptance)
+int acceptance = 100;// the euclidean distance acceptance variable (distance < acceptance)
 
-/**
- * Our method fired to start the motors. We set different variables for the motors and put them in the correct positions.
- */
-void startup() {
-  // turn on motor 1 (arm motor)
-  arm_motor.setSpeed(armSpeed); // set the speed to armSpeed
-  arm_motor.run(RELEASE); // no movement yet
-
-  // turn on motor 2 (slide motor)
-  slide_motor.setSpeed(slideSpeed); // set the speed to slideSpeed
-  slide_motor.run(RELEASE); // no movement yet
-
-  // Arm setup
-  // 
-  arm_motor.run(FORWARD);
-  delay(750);
-  arm_motor.run(BACKWARD);
-  delay(armDelay);
-  arm_motor.run(RELEASE);
-
-  // Slide setup
-  slide_motor.run(BACKWARD);
-  delay(750);
-  slide_motor.run(FORWARD);
-  delay(100);
-  slide_motor.run(RELEASE);
-}
+int checkInterval = 1100; // interval which the sensor checks in miliseconds
+int sensorDetectDelay = 600; // delay between sensing the disk and moving the arm / slide
+int errorCounter = 0;
 
 /**
  * Our method for moving the arm. Since it is protothreaded, we constantly fire it and first check whether the arm should move.
@@ -127,52 +109,6 @@ void slideMove() {
   }
 }
 
-/**
- * Distance calculated using the p-norm. Used by our function below to calculate the distance between the raw data and the preset colors.
- */
-float pNormDistance(uint16_t vector1[], uint16_t vector2[], int p) {
-  float x = 0;
-  int len1 = sizeof(vector1)/sizeof(vector1[0]);
-  int len2 = sizeof(vector2)/sizeof(vector2[0]);
-  if (len1 != len2) {
-    return -1;
-  }
-  for (int i = 0; i < len1; i++) {
-    x+= pow(abs(((float)vector1[i])-((float)vector2[i])),p);
-  }
-  return pow(x,1/p);
-}
-
-/**
- * Our method for getting the disk color based on the rgb values provided by the sensor.
- */
-int getColor(uint16_t r, uint16_t g, uint16_t b, uint16_t c, uint16_t lux) {
-  int p = 2;  // we use euclidean distance, but we could easily switch to manhattan or other set distances
-  uint16_t rawData[] = {r,g,b,c,lux}; // the rgb c and lux values of input in an array
-  uint16_t black[] = {115,120,100,0,80};  // the values of black 2
-  uint16_t green[] = {310,600,350,0,500};  // the values of green 3
-  uint16_t white[] = {1300,1750,1620,0,1000};  // the values of white 1
-  uint16_t belt[] = {0,52,33,0,0}; // the values of the belt 0
-  float blackDist = pNormDistance(rawData,black,p); // the euclidean distance between the sensor rgb and black
-  float greenDist = pNormDistance(rawData,green,p); // the euclidean distance between the sensor rgb and green
-  float whiteDist = pNormDistance(rawData,white,p); // the euclidean distance between the sensor rgb and white
-  float beltDist = pNormDistance(rawData,belt,p);  // the euclidean distance between the sensor rgb and belt
-  float minDist = min(min(beltDist,blackDist),min(greenDist,whiteDist));
-  if (minDist < acceptance) { // if there is a distance smaller than the acceptance variable
-    if (whiteDist == minDist) {  // if white has smallest distance to raw data
-      return 1; // disk is white
-    }
-    else if (blackDist == minDist) {  // if black has smallest distance to raw data 
-      return 2; // disk is black
-    }
-    else if (greenDist == minDist) {  // if green has smallest distance to raw data
-      return 3; // disk is green
-    }
-    else return 0;  // else belt has smallest distance to raw data by process of elimination, no disk present
-  }
-  else return -1; // if smallest distance is not smaller than acceptance, return -1 meaning something wrong is on the belt
-}
-
 TimedAction armThread = TimedAction(armFreq,armMove);       // TimedAction of the armMove function for protothreading
 TimedAction slideThread = TimedAction(slideFreq,slideMove); // TimedAction of the slideMove function for protothreading
 
@@ -181,13 +117,13 @@ TimedAction slideThread = TimedAction(slideFreq,slideMove); // TimedAction of th
  */
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Setup!");
+  Serial.println();Serial.println("Setup!");
 
   if (tcs.begin()) {
     Serial.println("Found sensor");
   } else {
-    Serial.println("No TCS34725 found ... check your connections");
-    while (1);
+    Serial.println("The TCS34725 color sensor cannot be found. \nPlease make sure you connected your sensors correctly. \nIf it is all connected, try replacing the wires.");
+    //while (1);
   }
 
   armEnable = true;
@@ -203,34 +139,66 @@ void setup() {
  */
 void loop() {
   uint16_t r, g, b, c, colorTemp, lux;
+  int color;
   
   armThread.check();  // check if the armThread has to be refired yet
   slideThread.check();  // check if the slideThread has to be refired yet
 
-  tcs.getRawData(&r, &g, &b, &c);
-  colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
-  lux = tcs.calculateLux(r, g, b);
+  if(millis() % checkInterval == 0) {
+    tcs.getRawData(&r, &g, &b, &c);
+    colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+    lux = tcs.calculateLux(r, g, b);
+  
+    color = getColor(r, g, b);
+  
+    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+    Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+    Serial.print("C: "); Serial.print(c, DEC); Serial.print(" : ");
+    Serial.println(color);
+    armThread.check();
+    slideThread.check();
 
-  int color = getColor(r, g, b, c, lux);
+    if(color == 2 && !(slideEnable || armEnable)) {
+      delay(sensorDetectDelay);
+      slideEnable = true;
+      armEnable = true;
+    } else if(color == 1) {
+      delay(sensorDetectDelay);
+      armEnable = true;
+    }
 
-  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" : ");
-  Serial.println(color);
-
-
-  if(color == 2) {
-    armEnable = true;
-    slideEnable = true;
-  } else if(color == 1) {
-    armEnable = true;
+    switch(color){
+    case 1:
+      delay(sensorDetectDelay);
+      armEnable = true;
+      errorCounter = 0;
+    break;
+    case 2:
+      delay(sensorDetectDelay);
+      slideEnable = true;
+      armEnable = true;
+      errorCounter = 0;
+    break;
+    case 3:
+      
+    break;
+    case -1:
+      errorCounter++;
+    break;
+    }
+  }
+  
+  if(errorCounter > 5) {
+    Serial.println("There was an error with the color sensor. \nColor not recognized with the disks...");
+    delay(100);
+    exit(1);
   }
   
   
-  if (millis()>25000) { // early exit call for testing purposes
+  if (millis()>60000) { // early exit call for testing purposes
     arm_motor.run(RELEASE);
     slide_motor.run(RELEASE);
     exit(0);
